ab19230e38edacd74b442ad4391fcad067854e3f
JabezJesudasonJena <170162059+JabezJesudasonJena@users.noreply.github.com>
2026-02-15 22:37:10 +0530

    Currect edits till nwo

---
+++ controllers/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const middleware = async (req, res , next) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            return res.status(400).json({message : "There is no authHeader"});
+        }
+        const token = authHeader.split(' ')[1];
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(500).json({message : err.message});
+    }
+}
+
+module.exports = middleware
